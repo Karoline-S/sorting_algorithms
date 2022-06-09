d3fd9ac858d0356842224155d18a1100a9c34b0f
declannoble <declanjnoble@hotmail.com>
2022-06-08 22:41:15 -0700

    fixed syntax and unused var

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,20 +1,29 @@
 #include "sort.h"
-
+/**
+ * bubble_sort - sorts an array of integers using a bubble
+ * sort algorithm
+ *
+ *@array: Pointer to an array
+ *
+ *@size: size of the array
+ *
+ *Return: nothing
+ */
 void bubble_sort(int *array, size_t size)
 {
-	int flag;
+	int flag = 1;
 	int tmp = 0;
-	ssize_t i;
+	size_t i;
 
 	if (size < 2 || array == NULL)
 		return;
-	while(1)
+	while(flag == 1)
 	{
 		flag = 0;
 
 		for (i = 0; i < size -1; i++)
 		{
-			if array[i] > array[i + 1]
+			if (array[i] > array[i + 1])
 				{
 					tmp = array[i];
 					array[i] = array[i + 1];
