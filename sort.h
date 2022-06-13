025ab4a8fb55db1f6087ac879383736edbe146dd
declannoble <declanjnoble@hotmail.com>
2022-06-12 21:20:57 -0700

    added prototypes

--- sort.h
+++ sort.h
@@ -28,5 +28,7 @@ void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void shell_sort(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
-
+void quickrec(int *array, int start, int end, size_t size);
+void swap(int *x, int *y);
+int partition(int *array, size_t start, size_t end, size_t size);
 #endif /* _SORT_H */
