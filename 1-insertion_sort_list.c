44f278372ffa8b7f55d54a13b639c784c5545352
Karoline-S <karoline.amy@gmai.com>
2022-06-13 17:18:23 -0700

    fix - add early return back in

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -12,7 +12,7 @@ void insertion_sort_list(listint_t **list)
 	listint_t *current;
 	listint_t *hold;
 
-	if (*list == NULL)
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
 
 	current = (*list)->next;
